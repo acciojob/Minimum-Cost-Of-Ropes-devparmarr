6a6afa049c9b0d42d12b5d2b4b035d7f7087b171
Dev Parmar <89907756+devparmarr@users.noreply.github.com>
2023-07-30 23:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function calculateMinCost() {
-  //your code here
-  let string = document.getElementById("rope-lengths").value
-	
-	let arr = string.split(",");
-	let finalValue =0;
-	while(arr.length > 1){
-		arr.sort((a,b) =>{return(a-b)});
-		let value = parseInt (arr.shift());
-		let value1 = parseInt(arr.shift());
-		let mainValue = value+value1;
-		finalValue = finalValue + mainValue;
-		arr.push(mainValue);
-		
-	}
+  let string = document.getElementById("rope-lengths").value;
+
+  // Validate input and filter out non-numeric and negative values
+  let arr = string.split(",").map(num => parseInt(num)).filter(num => !isNaN(num) && num >= 0);
+
+  let finalValue = 0;
+
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b);
+    let value = arr.shift();
+    let value1 = arr.shift();
+    let mainValue = value + value1;
+    finalValue = finalValue + mainValue;
+    arr.push(mainValue);
+  }
+
   let result = document.getElementById("result");
-	result.innerText = finalValue;
-	return finalValue;
-			
+  result.innerText = finalValue;
+  return finalValue;
 }
-  
-  
